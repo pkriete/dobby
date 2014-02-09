a180b50616ef5dcaa76739b0864b28584303fad8
Pascal Kriete <pascal@pascalkriete.com>
2014-02-09 16:59:59 -0500

    whitespace

--- lib/printer.rb
+++ lib/printer.rb
@@ -13,36 +13,35 @@ Configurations:
 EOS
 
 
-class Printer
-  class <<self
+module Printer
+  extend self
 
-    # Everyone loves colored output!
+  # Everyone loves colored output!
 
-    def red text; "\033[0;31m#{text}\033[0m"; end
-    def green text; "\033[0;92m#{text}\033[0m"; end
+  def red text; "\033[0;31m#{text}\033[0m"; end
+  def green text; "\033[0;92m#{text}\033[0m"; end
 
-    # Takes a 2d array [[key, value], [ke, va]] and prints
-    # it as two columns. TODO more than two columns?
-    #
-    def columns(data)
-      longest = data.map { |item| item[0].length}.sort.last
+  # Takes a 2d array [[key, value], [ke, va]] and prints
+  # it as two columns. TODO more than two columns?
+  #
+  def columns(data)
+    longest = data.map { |item| item[0].length}.sort.last
 
-      data.each do |item|
-        printf "%-#{longest+10}s %s\n", item[0], item[1]
-      end
+    data.each do |item|
+      printf "%-#{longest+10}s %s\n", item[0], item[1]
     end
+  end
 
-    # Show an error message
-    #
-    def error(msg)
-      puts "#{self.red 'Error:'}\033[0m #{msg}"
-      exit 1
-    end
+  # Show an error message
+  #
+  def error(msg)
+    puts "#{self.red 'Error:'}\033[0m #{msg}"
+    exit 1
+  end
 
-    # Show the help text
-    #
-    def help 
-      puts HELP_TEXT
-    end
+  # Show the help text
+  #
+  def help 
+    puts HELP_TEXT
   end
 end
